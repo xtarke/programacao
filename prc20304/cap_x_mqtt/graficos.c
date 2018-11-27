7648a24a1a5984f4e1fc4ec5f0c0e112d84072b1
Renan Augusto Starke <xtarke@gmail.com>
2018-11-27 16:49:43 -0200

    Added graficos routine

---
+++ prc20304/cap_x_mqtt/graficos.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#define ISCR 80//60
+#define JSCR 21
+#define BRANCO ' '
+#define ZERO '-'
+#define YY '|'
+#define XX '-'
+#define FF '.'
+
+/*********************************************************************
+   Para uso iterativo no terminal. Produz um gráfico simples de fun
+   para o intervalo x1, x2. Permite plotar outro gráfico até a satis-
+   fação do usuário.
+   Adaptado de:
+   PRESS, William H. Press et alii. Numerical recipes in C : the art
+   of scientific computing.Cambridge University Press, 1992
+*********************************************************************/
+
+typedef float (*ptr)(float);
+float fun(float);
+
+int main()
+
+{
+    int jz,j,i;
+    float ysml,ybig,x2,x1,x,dyj,dx,y[ISCR+1];
+    ptr fx = fun;
+    char scr[ISCR+1][JSCR+1];
+    setlocale(LC_CTYPE,"");
+    while(1)
+    {
+        printf("\nEntre com x1 e x2 separados por espaço\n");
+        printf("\nx1=x2 para parar:\n");
+        scanf("%f %f",&x1,&x2);
+        if (x1 == x2)
+            break;
+        for (j=1; j<=JSCR; j++)
+            scr[1][j]=scr[ISCR][j]=YY;
+        for (i=2; i<=(ISCR-1); i++)
+        {
+            scr[i][1]=scr[i][JSCR]=XX;
+            for (j=2; j<=(JSCR-1); j++)
+                scr[i][j]=BRANCO;
+        }
+        dx=(x2-x1)/(ISCR-1);
+        x=x1;
+        ysml=ybig=0.0;
+        for (i=1; i<=ISCR; i++)
+        {
+            y[i]=(*fx)(x);
+            if (y[i] < ysml) ysml=y[i];
+            if (y[i] > ybig) ybig=y[i];
+            x += dx;
+        }
+        if (ybig == ysml)
+            ybig=ysml+1.0;
+        dyj=(JSCR-1)/(ybig-ysml);
+        jz=1-(int) (ysml*dyj);
+        for (i=1; i<=ISCR; i++)
+        {
+            scr[i][jz]=ZERO;
+            j=1+(int) ((y[i]-ysml)*dyj);
+            scr[i][j]=FF;
+        }
+        printf(" %10.3f ",ybig);
+        for (i=1; i<=ISCR; i++)
+            printf("%c",scr[i][JSCR]);
+        printf("\n");
+        for (j=(JSCR-1); j>=2; j--)
+        {
+            printf("%12s"," ");
+            for (i=1; i<=ISCR; i++)
+                printf("%c",scr[i][j]);
+            printf("\n");
+        }
+        printf(" %10.3f ",ysml);
+
+        for (i=1; i<=ISCR; i++)
+            printf("%c",scr[i][1]);
+        printf("\n");
+        printf("%8s %10.3f %44s %10.3f\n"," ",x1," ",x2);
+    }
+    return 0;
+}
+float fun(float x)
+{
+    // sua função aqui
+    return x*x;
+} 
+
+
+
+
+
