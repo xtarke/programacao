e571d02b4017bb1138dadf4e9719dec8dbfce158
Renan Augusto Starke <xtarke@gmail.com>
2025-09-02 14:42:26 -0300

    Adicionado exemplo

---
+++ prg203402/exemplos/01_ola_mundo.cpp
@@ -0,0 +1,16 @@
+/* Programa básico que exibe "olá mundo" */
+
+/* Bibliotecas */
+#include <iostream>
+
+/* Namespace padrão */
+using namespace std;
+
+/* Função main: ponto de entrada */
+int main() {
+
+	// Imprime Olá mundo!!!
+	cout << "Olá mundo!!!\n";
+
+	return 0;
+}
