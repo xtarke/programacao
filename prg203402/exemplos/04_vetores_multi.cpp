55d949a524ae9b882199e1c653efa84890873993
Renan Augusto Starke <xtarke@gmail.com>
2025-10-14 17:49:13 -0300

    Adicionado exemplo com vetores.

---
+++ prg203402/exemplos/04_vetores_multi.cpp
@@ -0,0 +1,26 @@
+/* Programa básico que exibe "olá mundo" */
+
+/* Bibliotecas */
+#include <iostream>
+
+/* Namespace padrão */
+using namespace std;
+
+/* Função main: ponto de entrada */
+int main() {
+    int i = 0;
+    int j = 0;
+
+    int notas[10][3];
+
+    for (i=0; i < 10; i++)
+    {
+        cout << "Aluno: " << i << endl;;
+        for (j=0; j < 3; j++){
+            cout << "Nota[" << j << "]: ";
+            cin >> notas[j][i];
+        }
+    }
+
+	return 0;
+}
