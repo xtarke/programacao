b9dd7437e169502b9f3b0bd482c697a9968b5a97
Renan Augusto Starke <xtarke@gmail.com>
2025-11-11 12:52:50 -0300

    Added samples.

---
+++ prg203402/exemplos/07_funcoes_01.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : p1_bruno.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : P1
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void imprimi_dobro_funcao(int x){
+
+	cout << "Dobro: " << 2 * x << endl;
+
+}
+
+
+int main() {
+
+	imprimi_dobro_funcao(6);
+	imprimi_dobro_funcao(7);
+	imprimi_dobro_funcao(25);
+
+
+	return 0;
+}
---
+++ prg203402/exemplos/07_funcoes_02.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : p1_bruno.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : P1
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void minha_funcao(){
+
+	cout << "Olá, sou uma função." << endl;
+
+}
+
+int main() {
+
+	minha_funcao();
+	minha_funcao();
+	minha_funcao();
+
+	return 0;
+}
