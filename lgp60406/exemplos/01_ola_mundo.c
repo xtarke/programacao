5505e6791de0171e3ac7be444ac1bcd9526d2985
Renan Augusto Starke <xtarke@gmail.com>
2026-08-03 13:15:41 -0300

    Added lgp604

---
+++ lgp60406/exemplos/01_ola_mundo.c
@@ -0,0 +1,14 @@
+/* Programa básico que exibe "olá mundo" */
+
+/* Bibliotecas */
+#include <stdio.h>
+
+
+/* Função main: ponto de entrada */
+int main() {
+
+	// Imprime Olá mundo!!!
+	printf("Olá mundo!!!\n");
+
+	return 0;
+}
